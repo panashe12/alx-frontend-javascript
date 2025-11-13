37135f8e54b9c251e819bdb3062c77e073751b35
panashe12 <panashekanyai@gmail.com>
2025-11-13 23:54:24 +0200

    4rth

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,33 +1,36 @@
+// Teacher interface
 interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [key: string]: any;
+  readonly firstName: string;        // can only be set at initialization
+  readonly lastName: string;         // can only be set at initialization
+  fullTimeEmployee: boolean;         // must always be defined
+  yearsOfExperience?: number;        // optional
+  location: string;                  // must always be defined
+  [key: string]: any;                // allow extra attributes
 }
 
+// Directors interface extends Teacher
 interface Directors extends Teacher {
-  numberOfReports: number;
+  numberOfReports: number;           // required for Directors
 }
 
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-
-console.log(director1);
-
+// Example Teacher object
 const teacher3: Teacher = {
   firstName: 'John',
   lastName: 'Doe',
   fullTimeEmployee: false,
   location: 'London',
-  contract: false,
+  contract: false,                   // extra attribute allowed
 };
 
-console.log(teacher3);
+console.log('Teacher:', teacher3);
 
+// Example Director object
+const director1: Directors = {
+  firstName: 'Jane',
+  lastName: 'Smith',
+  location: 'New York',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log('Director:', director1);
