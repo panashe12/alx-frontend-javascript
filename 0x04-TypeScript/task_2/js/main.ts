4796852703fcde286c072e63443e883a37bc6c9e
panashe12 <panashekanyai@gmail.com>
2025-11-15 01:55:04 +0200

    2

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -59,3 +59,21 @@ function createEmployee(salary: number | string): Director | Teacher {
 console.log(createEmployee(200).constructor.name);  // Teacher
 console.log(createEmployee(1000).constructor.name); // Director
 console.log(createEmployee("$500").constructor.name); // Director
+
+// Type predicate to check if employee is Director
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+// Function to execute the correct work based on employee type
+function executeWork(employee: Director | Teacher): void {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
+// Testing
+executeWork(createEmployee(200));  // Getting to work
+executeWork(createEmployee(1000)); // Getting to director tasks
